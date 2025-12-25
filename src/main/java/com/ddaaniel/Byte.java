db95b63fb5efe12e82a9f68d35ce2a73bc613122
ddaaniel <daniel0333v@gmail.com>
2025-12-25 12:17:13 -0300

    feat(demo): libraries try out

---
+++ src/main/java/com/ddaaniel/Byte.java
@@ -0,0 +1,90 @@
+package com.ddaaniel;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.matcher.ElementMatchers;
+
+class Oh {
+  private static final Logger log = Logger.getLogger(Byte.class.getName());
+
+  public void donothing() {
+    log.info("nothing");
+  }
+}
+
+public class Byte {
+
+  private static final Logger log = Logger.getLogger(Byte.class.getName());
+
+  /*
+   *
+   * [.java ] → [Compilador javac] → [Bytecode .class] →
+   * [JVM Carrega] → [Metadados na Memória] → [Reflection
+   * acessa]
+   *
+   * Metadados refere-se principalmente às informações sobre as
+   * classes (como definições, métodos, constantes) armazenadas
+   * na área de memória chamada Metaspace (ou PermGen antes do
+   * Java 8), essenciais para o carregamento, verificação e
+   * execução de classes, sendo vitais para o funcionamento interno
+   * da JVM e para recursos como reflexão.
+   *
+   */
+  public void doReflection() {
+    Object typeReference = null;
+
+    try {
+      typeReference = Class.forName("com.ddaaniel.Oh").getDeclaredConstructor().newInstance();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    for (Method m : typeReference.getClass().getMethods()) {
+      // var modifier = m.getModifiers();
+      var returnType = m.getReturnType();
+      var accessFlag = m.accessFlags();
+      var parameters = m.getParameters();
+
+      log.info(String.format("%s %s %s Params: %s",
+          accessFlag,
+          returnType.getSimpleName(),
+          m.getName(),
+          Arrays.toString(parameters)));
+    }
+  }
+
+  public static void main(String[] args) {
+
+    // bytecode manipulation (com ByteBuddy)
+    // Modifica o bytecode ANTES da classe ser carregada
+    new ByteBuddy()
+        .subclass(Object.class)
+        // .name("com.ddaaniel.DynamicClass")
+        .method(ElementMatchers.named("toString"))
+        .intercept(FixedValue.value("Intercepted by ByteBuddy!"))
+        .make()
+        .load(Byte.class.getClassLoader())
+        .getLoaded();
+
+    /*
+     *
+     * When we use the 'Object.getClass();', we got a reference to an
+     * common Object java.lang.Class subjected to Garbage Collector,
+     * which resides on the Heap, that points to Metaspace structure
+     * inside of JVM (HotSpot) which contains the structure calls
+     * 'InstanceKlass' (memória nativa/off-heap), and there are placed
+     * the real metadata's like: class names, methods, fields and
+     * bytecode. When requested via '.getName()' or '.toString()', the
+     * object on the Heap consults this structure 'InstanceKlass' on
+     * Metaspace to recovery the class name string.
+     *
+     */
+    log.info(new Oh().getClass().toString());
+
+    new Byte().doReflection();
+  }
+}
